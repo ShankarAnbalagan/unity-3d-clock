b59c2c0a4a4d68108c51e37bad5adde9f53d1a1c
Shankar Anbalagan <bravoshankar@gmail.com>
2017-11-28 12:45:11 +0530

    Initial commit

---
+++ Assets/clock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clock : MonoBehaviour {
+
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+
+    void Awake()
+    {
+        hoursTransform.localRotation = Quaternion.Euler(0f,DateTime.Now.Hour*30f,0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Minute*6f, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Second*6f, 0f);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hoursTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Hour*30f, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Minute*6f, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Second*6f, 0f);
+
+    }
+}
